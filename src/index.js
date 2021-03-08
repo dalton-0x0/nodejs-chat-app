341bec607aa4006de623b75371acd18c8e9dfd26
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-08 13:45:22 -0700

    set up initial socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app); 
+const io = socketio(server)
+// express() auto creates a http server but socket.io doesn't
+// setup required on the client side as well before io.on() connection
+
+// define port number
 const port = process.env.PORT || 3000;
 
-// Define paths for Express config
+// define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 
-// Setup static directory to serve
+// setup static directory to serve up
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+    console.log("chat-app socket.io connection");
+});
+
+server.listen(port, () => {
     console.log("chat-app server is up on port " + port);
 });

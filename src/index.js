023b1db78845ddb8a378c3aa157e0f3a9f92c6f4
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-08 20:02:09 -0700

    duplex connection to send and receive message

--- src/index.js
+++ src/index.js
@@ -9,24 +9,17 @@ const io = socketio(server)
 // express() auto creates a http server but socket.io doesn't
 // setup required on the client side as well before io.on() connection
 
-// define port
 const port = process.env.PORT || 3000;
-
-// define path to public for html render
 const publicDirectoryPath = path.join(__dirname, "../public");
-
-// setup static directory to serve up
 app.use(express.static(publicDirectoryPath));
-
-let count = 0;
 
 io.on("connection", (socket) => {
     console.log("socket.io server connection");
-    socket.emit("countUpdated", count);
-    socket.on("increment", () => {
-        count++;
-        // socket.emit("countUpdated", count); // emits to only the 'socket' connection
-        io.emit("countUpdated", count); // emits to every connection
+
+    socket.emit("message", "welcome to chat");
+
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);
     });
 });
 

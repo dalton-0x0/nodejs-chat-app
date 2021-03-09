4b314c84d6aa8919c4d207e9af408e287d805f09
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-08 21:13:59 -0700

    socket sendLocation and return google maps url

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app); 
-const io = socketio(server)
-// express() auto creates a http server but socket.io doesn't
-// setup required on the client side as well before io.on() connection
+const io = socketio(server) // express() auto creates a http server but socket.io doesn't
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -17,9 +15,21 @@ io.on("connection", (socket) => {
     console.log("socket.io server connection");
 
     socket.emit("message", "welcome to chat");
+    socket.broadcast.emit("message", "new user joined chat");
 
     socket.on("sendMessage", (message) => {
         io.emit("message", message);
+    });
+
+    socket.on("sendLocation", (coords) => {
+        io.emit(
+            "message",
+            `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+        );
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("message", "a user has left");
     });
 });
 

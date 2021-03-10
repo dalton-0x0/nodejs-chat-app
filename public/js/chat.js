c7005189d9ec1a6772e6c5b88c2aed563b668efa
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-10 07:42:13 -0700

    rendering chat message in html body

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,21 @@
 const socket = io();
 
+// html messages
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
-// const $messages = document.querySelector('#messages')
+const $messages = document.querySelector("#messages");
+
+// handlebars templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
 
 socket.on("message", (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message,
+    });
+    $messages.insertAdjacentHTML("beforeend", html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
